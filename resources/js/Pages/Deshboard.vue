1b441a05460465087b2b629e5356e9316de2859e
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-15 12:10:00 +0600

    Authencation Vue

---
+++ resources/js/Pages/Deshboard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from '../Layouts/AppLayout.vue';
+
+
+
+ 
+</script>
+ 
+<template>
+ <AppLayout >
+   <h4 class="text-4xl text-center font-bold ">Wellcome to Vue Simple Authentication</h4>
+ </AppLayout>
+</template>
+ 
+<style scoped>
+ 
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from '../Layouts/AppLayout.vue';
+
+
+
+ 
+</script>
+ 
+<template>
+ <AppLayout >
+    <h1>Hello boss</h1>
+ </AppLayout>
+</template>
+ 
+<style scoped>
+ 
+</style>
